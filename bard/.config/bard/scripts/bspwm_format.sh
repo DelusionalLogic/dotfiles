ca68359666deeb6d372196ef6167d476b74095e0
Jesper Jensen <jesper@slashwin.dk>
2018-08-22 00:33:13 +0200

    "New" rice

--- bard/.config/bard/scripts/bspwm_format.sh
+++ bard/.config/bard/scripts/bspwm_format.sh
@@ -1,3 +1,82 @@
+function tag() {
+	case ${i:0:1} in
+		'm')
+			echo "["
+			return
+			;;
+		'M')
+			echo "["
+			return
+			;;
+		'L')
+			echo "]"
+			return
+			;;
+		'T')
+			return
+			;;
+		'G')
+			return
+			;;
+	esac
+
+	workspace_name="${i:1}"
+
+	local oldifs=$IFS
+	IFS=$'\n'
+
+	workspace_icon=""
+
+	for w in $(bspc query -N -n .window -d "$workspace_name"); do
+		classes=$(xprop -id $w WM_CLASS | cut -c 20-)
+		if [[ $classes = *"Firefox"* ]]; then
+			workspace_icon=" %{T$icon}\uE1BF%{T-} "
+		fi
+
+		if [[ $classes = *"Termite"* ]]; then
+			workspace_icon=" %{T$icon}\uE1EF%{T-} "
+		fi
+
+		if [[ $classes = *"twitch"* ]]; then
+			workspace_icon=" %{T$icon}\uE1D3%{T-} "
+		fi
+
+		if [[ $classes = *"google play music"* ]]; then
+			workspace_icon=" %{T$icon}\uE04D%{T-} "
+		fi
+
+		if [[ $classes = *"Steam"* ]]; then
+			workspace_icon=" %{T$icon}\uE1CE%{T-} "
+		fi
+	done
+
+	if [[ ! -z $workspace_icon ]]; then
+		echo -en $workspace_icon
+		return
+	fi
+
+	IFS=$oldifs
+
+	# bspc query -N -n .window -d 2 | parallel xprop -notype -id {} WM_CLASS
+	case ${i:0:1} in
+		'F')
+			echo -en " %{T$icon}\uE1BC%{T-} "
+			;;
+		'f')
+			echo -en " %{T$icon}\uE1BC%{T-} "
+			;;
+		'o')
+			echo -en " %{T$icon}\uE1C2%{T-} "
+			;;
+		'O')
+			echo -en " %{T$icon}\uE1C2%{T-} "
+			;;
+		*)
+			continue
+			;;
+	esac
+}
+
 IFS=$':'
 grpcol="$inactive_col"
 echo "$input" >&2
@@ -9,37 +88,32 @@ for i in $input ; do
 		i=${i:1:-1}
 		first=true
 	fi
+	tagstr=$(tag $i)
 	case ${i:0:1} in
 		'F')
-			tagfgcol="$inactive_col"
-			tagstr=" + "
+			tagfgcol="$active_col"
 			;;
 		'f')
 			tagfgcol="$inactive_col"
-			tagstr=" - "
 			;;
 		'o')
-			tagfgcol="$active_col"
-			tagstr=" - "
+			tagfgcol="$inactive_col"
 			;;
 		'O')
 			tagfgcol="$active_col"
-			tagstr=" + "
 			;;
 		'm')
 			[ $first = false ] && echo -n " "
 			tagfgcol="$inactive_col"
-			tagstr="["
 			grpcol="$inactive_col"
 			;;
 		'M')
 			[ $first = false ] && echo -n " "
 			tagfgcol="$sep_col"
-			tagstr="["
 			grpcol="$sep_col"
 			;;
 		'L')
-			echo -n "%{F$grpcol}]"
+			tagfgcol="$grpcol"
 			;;
 		*)
 			continue
