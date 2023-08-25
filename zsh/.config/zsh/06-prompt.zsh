e95361f4ae6daf764a00c7f5ba80c124a9b1e88e
Jesper Jensen <jesper@jnsn.dev>
2023-08-26 00:13:00 +0200

    Remove the ruby dep

--- zsh/.config/zsh/06-prompt.zsh
+++ zsh/.config/zsh/06-prompt.zsh
@@ -5,11 +5,24 @@ export FMT_BRANCH="%F{2}(%s:%F{7}%{$IT%}%r%{$ST%}%F{2}) %F{11}%B%b %K{0}%{$IT%}%
 export FMT_ACTION="(%F{3}%a%f)"
 export FMT_PATH="%F{1}%R%F{2}/%S%f"
 
+function make_path() {
+	local dir="${PWD/#\/home\/delusional/~/}"
+	local parts=(${(s|/|)dir})
+	local color=1
+	for part in "${parts[@]}"; do
+		if [[ "$color" -gt 1 ]];  then
+			printf "%%F{$color} \u00BB "
+		fi
+		printf "%%F{8}$part"
+		((color+=1))
+	done
+}
+
 function setprompt() {
 	local MODE="${${KEYMAP/vicmd/"%F{2}n%f"}/(main|viins)/"%F{4}i%f"}"
 	local HOST="%F{1}$HOST%f"
 	local USER="%(#.%F{1}.%F{3})%n%f"
-	local PWD="%F{7}$($XDG_CONFIG_HOME/zsh/rzsh_path)%f"
+	local PWD="%F{7}$(make_path)%f"
 	local EXIT="%(?..%F{202}%?%f)"
 	local PRMPT="${USER}@${HOST}:${MODE}: ${PWD} 
 ${EXIT} %F{202}»%f "
